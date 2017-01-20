e00e31d84f53f3349a1e8eceb39e1fd84e44b4fa
Allen Rohner <arohner@gmail.com>
2017-01-19 18:31:00 -0600

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject arohner/clj-kube "0.1.1-SNAPSHOT"
+(defproject arohner/clj-kube "0.1.1"
   :dependencies [[com.cemerick/url "0.1.1"]
                  [clj-http "2.3.0"]
                  [org.clojure/core.incubator "0.1.4"]
