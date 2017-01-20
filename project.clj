1e8d9b4ea9edfa91216fb8b0362a37acfc399fdc
Allen Rohner <arohner@gmail.com>
2017-01-19 18:28:23 -0600

    Proper namespaced project

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject clj-kube "0.1.0-SNAPSHOT"
+(defproject arohner/clj-kube "0.1.0-SNAPSHOT"
   :dependencies [[com.cemerick/url "0.1.1"]
                  [clj-http "2.3.0"]
                  [org.clojure/core.incubator "0.1.4"]
-                 [org.clojure/clojure "1.8.0"]])
+                 [org.clojure/clojure "1.8.0"]]
+  :deploy-repositories [["releases" :clojars]])
