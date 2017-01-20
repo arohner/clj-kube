5258c9b2143ff7370f81f2a7b1fdbcdaf58fd6eb
Allen Rohner <arohner@gmail.com>
2017-01-19 18:30:47 -0600

    Let's try again

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject arohner/clj-kube "0.1.0"
+(defproject arohner/clj-kube "0.1.1-SNAPSHOT"
   :dependencies [[com.cemerick/url "0.1.1"]
                  [clj-http "2.3.0"]
                  [org.clojure/core.incubator "0.1.4"]
