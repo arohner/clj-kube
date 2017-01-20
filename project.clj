2a1fbe2d93084938ba42c16c937fe62978066e7c
Allen Rohner <arohner@gmail.com>
2017-01-19 18:26:08 -0600

    Missing project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-kube "0.1.0-SNAPSHOT"
+  :dependencies [[com.cemerick/url "0.1.1"]
+                 [clj-http "2.3.0"]
+                 [org.clojure/core.incubator "0.1.4"]
+                 [org.clojure/clojure "1.8.0"]])
