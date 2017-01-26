edb6bbc605354927834cb8b1a13d434848bfff46
Allen Rohner <arohner@gmail.com>
2017-01-26 14:33:37 -0600

    Missing dep

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject arohner/clj-kube "0.1.2-SNAPSHOT"
   :dependencies [[com.cemerick/url "0.1.1"]
                  [clj-http "2.3.0"]
+                 [cheshire "5.6.3"]
                  [org.clojure/core.incubator "0.1.4"]
                  [org.clojure/clojure "1.8.0"]]
   :deploy-repositories [["releases" :clojars]])
