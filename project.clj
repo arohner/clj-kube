1e845e3f03734ebc94f7350c1929a5024b16f6d7
Allen Rohner <arohner@gmail.com>
2017-04-03 12:38:47 -0500

    Version 0.1.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject arohner/clj-kube "0.1.7"
+(defproject arohner/clj-kube "0.1.8-SNAPSHOT"
   :dependencies [[com.cemerick/url "0.1.1"]
                  [clj-http "2.3.0"]
                  [cheshire "5.6.3"]
